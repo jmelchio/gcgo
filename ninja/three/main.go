86e2b560f34c625b49f33aa782e7c82a7d6cfdb2
Joris Melchior <joris.melchior@gmail.com>
2017-12-07 21:20:55 -0500

    exercises level 3 all done

--- ninja/three/main.go
+++ ninja/three/main.go
@@ -10,6 +10,10 @@ func main() {
 	printalive(1964)
 	printaliveagain(1964)
 	printremainder(10, 100, 4)
+	thisorthat("", "that")
+	fmt.Println(simpleswitch("I don't know"))
+	fmt.Println(stringswitch("sumtin"))
+	truethat()
 }
 
 // exercise #1
@@ -55,6 +59,55 @@ func printremainder(from, to, divisor int) {
 	for i := from; i <= to; i++ {
 		fmt.Printf("%d mod %d = %d\n", i, divisor, i%divisor)
 	}
+}
+
+// exercise #6 and #7
+func thisorthat(this, that string) {
+	if len(this) == 0 && len(that) == 0 {
+		fmt.Println("neither this nor that")
+	} else if len(this) == 0 && len(that) != 0 {
+		fmt.Println("not this but that")
+	} else if len(this) != 0 && len(that) == 0 {
+		fmt.Println("this but not that")
+	} else {
+		fmt.Println("this and that")
+	}
+}
+
+// exercise #8
+func simpleswitch(word string) string {
+	switch {
+	case word == "whoot":
+		return "why whoot?"
+	case word == "word":
+		return "word dude!"
+	case len(word) == 0:
+		return "sup quiet one?"
+	default:
+		return "what you said man!"
+	}
+}
+
+// exercise #9
+func stringswitch(identifier string) string {
+	switch identifier {
+	case "word":
+		return "word up"
+	case "no way":
+		return "why not?"
+	default:
+		return "whatever ..."
+	}
+}
+
+// exercise #10
+func truethat() {
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	fmt.Println(!false)
 }
 
 // That's All Folks !!
