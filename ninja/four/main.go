02730d4df85daf383ebedf9a15b959040b5fc0a4
Joris Melchior <joris.melchior@gmail.com>
2018-02-05 23:06:54 -0500

    First exercise done

--- ninja/four/main.go
+++ ninja/four/main.go
@@ -14,8 +14,19 @@ func main() {
 	}
 
 	for k, v := range mymap {
-		fmt.Printf("The age of %s is: %d", k, v)
+		fmt.Printf("The age of %s is: %d\n", k, v)
 	}
+
+	exerciseOne()
+}
+
+func exerciseOne() {
+	intArray := [5]int{1, 3, 5, 7, 9}
+
+	for i, v := range intArray {
+		fmt.Printf("Position: %d - Value: %d, Type: %T\n", i, v, v)
+	}
+	fmt.Printf("Type of variable is: %T\n", intArray)
 }
 
 // That's All Folks !!
