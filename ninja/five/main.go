caea16f249f1c895387628f0a770f8fbf47973f6
Joris Melchior <joris.melchior@gmail.com>
2018-02-10 13:22:47 -0500

    Exercise three and four done

--- ninja/five/main.go
+++ ninja/five/main.go
@@ -8,8 +8,25 @@ type person struct {
 	favoriteIcFlavors []string
 }
 
+type vehicle struct {
+	doors int
+	color string
+}
+
+type truck struct {
+	vehicle
+	fourWheel bool
+}
+
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
 func main() {
 	exerciseOne()
+	exerciseThree()
+	exerciseFour()
 
 	fmt.Println("That's all folks!!")
 }
@@ -35,4 +52,43 @@ func exerciseOne() {
 			fmt.Printf("\t%s\n", flavor)
 		}
 	}
+}
+
+func exerciseThree() {
+	aTruck := truck{
+		vehicle: vehicle{
+			doors: 2,
+			color: "white",
+		},
+		fourWheel: true,
+	}
+
+	aSedan := sedan{
+		vehicle: vehicle{
+			doors: 4,
+			color: "blue",
+		},
+		luxury: true,
+	}
+
+	fmt.Printf("A vehicle of type %T, with %d doors %s color and fourWheelDrive: %v\n",
+		aTruck, aTruck.doors, aTruck.color, aTruck.fourWheel)
+	fmt.Printf("A vehicle of type %T, with %d doors %s color and luxury equipped: %v\n",
+		aSedan, aSedan.doors, aSedan.color, aSedan.luxury)
+
+}
+
+func exerciseFour() {
+	anonymous := struct {
+		somestuff  string
+		otherstuff string
+		really     bool
+	}{
+		somestuff:  "some",
+		otherstuff: "other",
+		really:     true,
+	}
+
+	fmt.Printf("Sometimes %s is going on\nand then other times it's %s\n\t it's really %v\n",
+		anonymous.somestuff, anonymous.otherstuff, anonymous.really)
 }
