0a0fa041aa68d65b4d5fe6d4f75258f00e1fdd5b
Joris Melchior <joris.melchior@gmail.com>
2018-02-19 11:41:33 -0500

    Level seven complete

--- ninja/seven/main.go
+++ ninja/seven/main.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
 func main() {
-	fmt.Println("vim-go")
+	showPointer()
+	somePerson := person{
+		first: "John",
+		last:  "Doe",
+		age:   12,
+	}
+	fmt.Println(somePerson)
+	changeMe(&somePerson)
+	fmt.Println(somePerson)
+	fmt.Println("That's All Folks!!")
+}
+
+func showPointer() {
+	someVar := "This is a var"
+	fmt.Printf("%s, at address %v\n", someVar, &someVar)
+}
+
+func changeMe(p *person) {
+	p.age = 53
+	p.first = "Joris"
+	p.last = "Melchior"
 }
