bb65eef871739049fcdc235f5406e3d8831b5e01
Joris Melchior <joris.melchior@gmail.com>
2018-02-19 23:24:05 -0500

    More experiments on level eight

--- ninja/eight/main.go
+++ ninja/eight/main.go
@@ -4,10 +4,46 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 )
+
+type Person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+type ByFirst []Person
+
+func (n ByFirst) Len() int {
+	return len(n)
+}
+
+func (n ByFirst) Swap(i, j int) {
+	n[i], n[j] = n[j], n[i]
+}
+
+func (n ByFirst) Less(i, j int) bool {
+	return n[i].First < n[j].First
+}
+
+type ByLast []Person
+
+func (n ByLast) Len() int {
+	return len(n)
+}
+
+func (n ByLast) Swap(i, j int) {
+	n[i], n[j] = n[j], n[i]
+}
+
+func (n ByLast) Less(i, j int) bool {
+	return n[i].Last < n[j].Last
+}
 
 func main() {
 	simpleMarshal()
+	sortDemo()
 	fmt.Println("That's all for Ninja level eight folks !!")
 }
 
@@ -21,4 +57,35 @@ func simpleMarshal() {
 		os.Stdout.WriteString("\n")
 	}
 
+}
+
+func sortDemo() {
+	p1 := Person{
+		First: "Joris",
+		Last:  "Melchior",
+		Age:   53,
+	}
+	p2 := Person{
+		First: "Andrea",
+		Last:  "Domize",
+		Age:   52,
+	}
+	p3 := Person{
+		First: "Tom",
+		Last:  "Boduch",
+		Age:   58,
+	}
+	p4 := Person{
+		First: "Hiram",
+		Last:  "Lee",
+		Age:   48,
+	}
+
+	ps := []Person{p1, p2, p3, p4}
+
+	fmt.Println(ps)
+	sort.Sort(ByFirst(ps))
+	fmt.Println(ps)
+	sort.Sort(ByLast(ps))
+	fmt.Println(ps)
 }
