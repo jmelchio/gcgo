e8a70b68f5cc0623ad7343cbacef78d2dba1e5bb
Joris Melchior <joris.melchior@gmail.com>
2018-03-02 22:24:37 -0500

    Ninja level eleven exercise one

--- ninja/eleven/main.go
+++ ninja/eleven/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
 
 func main() {
+	exerciseOne()
 	fmt.Println("That's all for Ninja eleven folks !!")
+}
+
+func exerciseOne() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := json.Marshal(p1)
+	if err != nil {
+		log.Println("An error ocurred:", err)
+		return
+	}
+	fmt.Println(string(bs))
 }
